67170220d4ab409b9ad50b24051e638c9ce364da
Cao Binh <binhcao18@gmail.com>
2021-06-03 15:23:48 +0700

    add Leapyear

---
+++ IntroductionToJava/LeapYear/src/LeapYear.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year;
+        System.out.println("Nhập năm:");
+        year = scanner.nextInt();
+
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.printf("%d là năm nhuận", year);
+        } else {
+            System.out.printf("%d không phải là năm nhuận", year);
+        }
+    }
+}
+
