120d8b153d5108b5072745255dac5f0b9c679408
Cao Binh <binhcao18@gmail.com>
2021-06-04 10:10:41 +0700

    add isPrime and Intersetrate

--- LoopToJava/isPrime/src/isPrime.java
+++ LoopToJava/isPrime/src/isPrime.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class isPrime {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number < 2) {
+            System.out.println(number + " is not a prime");
+        } else {
+            boolean check = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    check = false;
+                    break;
+                }
+            } if (check) {
+                System.out.println(number + " is a number");
+            } else {
+                System.out.println(number + " is not a prime");
+            }
+        }
+    }
 }
